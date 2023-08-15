d887cf3217629f55cc393c266aca9539c04e7831
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2023-08-15 16:05:47 +0600

    updated product info show  layout

--- app/src/main/java/com/example/prismmart/Homepage/UI/productInfo.java
+++ app/src/main/java/com/example/prismmart/Homepage/UI/productInfo.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.prismmart.R;
 
-public class Design_info extends AppCompatActivity {
+public class productInfo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_design_info);
+        setContentView(R.layout.activity_product_info);
     }
 }
