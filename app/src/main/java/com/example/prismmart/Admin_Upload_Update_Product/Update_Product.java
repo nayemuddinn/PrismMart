ff6ad47876efbbc821dc715e02045c8de701669d
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2023-08-18 23:17:51 +0600

    View Product button in Update Activity

--- app/src/main/java/com/example/prismmart/Admin_Upload_Update_Product/Update_Product.java
+++ app/src/main/java/com/example/prismmart/Admin_Upload_Update_Product/Update_Product.java
@@ -6,17 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.prismmart.Homepage.UI.Homepage;
+import com.example.prismmart.Model.cartModel;
 import com.example.prismmart.Model.popularProductModel;
 import com.example.prismmart.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -24,9 +31,16 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 public class Update_Product extends AppCompatActivity implements View.OnClickListener {
 
-    EditText productID;
-    Button update, delete;
+    EditText productID, productName, productDescription, productPrice;
+    ImageView productImage;
+    Button update, delete, ViewProduct;
+    AutoCompleteTextView productCategory;
+    AutoCompleteTextView productUnit;
+    ArrayAdapter<String> product_cat_adapter;
+    ArrayAdapter<String> product_unit_adapter;
+    String[] categoryName, Unitname;
     FirebaseFirestore fstore;
+    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +50,31 @@ public class Update_Product extends AppCompatActivity implements View.OnClickLis
         productID = findViewById(R.id.update_product_ID);
         update = findViewById(R.id.update_update_product);
         delete = findViewById(R.id.update_product_delete);
+        ViewProduct = findViewById(R.id.update_product_View);
+        productDescription = findViewById(R.id.update_product_description);
+        productPrice = findViewById(R.id.update_product_price);
+        productCategory = findViewById(R.id.update_product_catagory);
+        productUnit = findViewById(R.id.update_product_unit_type);
+        productImage = findViewById(R.id.update_image_view);
+        productName = findViewById(R.id.update_product_name);
 
 
         delete.setOnClickListener(this);
+        ViewProduct.setOnClickListener(this);
         fstore = FirebaseFirestore.getInstance();
+
+
+        //category List autoCompleteTextview
+        categoryName = getResources().getStringArray(R.array.category_name);
+        product_cat_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, categoryName);
+        productCategory.setThreshold(1);
+        productCategory.setAdapter(product_cat_adapter);
+
+        //Product Unit autoCompleteTextView
+        Unitname = getResources().getStringArray(R.array.product_Unit_name);
+        product_unit_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, Unitname);
+        productUnit.setThreshold(1);
+        productUnit.setAdapter(product_unit_adapter);
 
 
     }
@@ -75,6 +110,30 @@ public class Update_Product extends AppCompatActivity implements View.OnClickLis
             });
         }
 
+        if (view.getId() == R.id.update_product_View) {
+            fstore.collection("All Product").whereEqualTo("productID", productID.getText().toString()).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if (task.isSuccessful() && !task.getResult().isEmpty()) {
+
+                        DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
+
+                        productName.setText(documentSnapshot.getString("productName").toString());
+                        productCategory.setText(documentSnapshot.getString("productCategory").toString());
+                        productUnit.setText(documentSnapshot.getString("productUnit").toString());
+                        productDescription.setText(documentSnapshot.getString("productDescription").toString());
+                        productPrice.setText(documentSnapshot.getString("productPrice").toString());
+                        Glide.with(Update_Product.this).load(documentSnapshot.getString("productImage").toString()).into(productImage);
+
+
+                    } else {
+                        Toast.makeText(Update_Product.this, "Product Not Found", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+
+        }
     }
 
     @Override
